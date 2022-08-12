10d40a2273d611fb039c4648fccb8c1905b7ef06
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-08-12 23:00:10 +0300

    add files

--- lesson-16/redux-toolkit/src/redux/books/books-selectors.js
+++
@@ -1,4 +0,0 @@
-export const getAllBooks = state => state.books.items;
-export const getBooksLoading = state => state.books.loading;
-
-export const getFavoriteBooks = ({books}) => books.items.filter(item => item.favorite);
--- lesson-16/redux-toolkit/src/redux/cart/cart-selectors.js
+++
@@ -1,5 +0,0 @@
-export const getCart = state => state.cart;
-
-export const getCartCount = state => state.cart.length;
-
-export const getCartTotalPrice = ({cart}) => cart.reduce((acum, item)=> acum + item.price, 0)
